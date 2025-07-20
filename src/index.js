b35344ee667ffd59a33be664e43f0cf26811a3ba
chitrarasu <chitrarasuchitrarasu97@gmail.com>
2025-07-20 21:09:23 +0530

    folder should be added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import '@fontsource/poppins';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import './styles/GlobalStyles.css';
+import { store } from './app/store';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
